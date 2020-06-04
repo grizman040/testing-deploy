7a50c45b62e6ca86e555ce13dab33f4f02f61f70
grizman040 <54029616+grizman040@users.noreply.github.com>
2020-06-04 10:29:26 +0200

    Updates

---
+++ precache-manifest.ca06643fd20580f2872e9ff29ca485ec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f860d8592fc23366e709e29b6a439dd",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c521c1d885ec874873bb",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1a0f615a8d8f38f773a6",
+    "url": "./static/js/2.be3e113b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.be3e113b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c521c1d885ec874873bb",
+    "url": "./static/js/main.2fb01f31.chunk.js"
+  },
+  {
+    "revision": "13d2e97a56c9681c85d8",
+    "url": "./static/js/runtime-main.40d6f1cf.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
